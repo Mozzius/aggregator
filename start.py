33856d7b3007677706b4384d22a17a6f50ed5847
Mozzius <mozzius.iii@gmail.com>
2018-03-30 23:53:04 +0100

    lil bit of login stuff

--- start.py
+++ start.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, redirect, session, request
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 import db
 
 app = Flask(__name__)
+login = LoginManager(app)
 
 @app.route('/')
 @app.route('/r/<subname>')
@@ -23,11 +25,22 @@ def sub(subname='frontpage',sort='hot'):
     return render_template('roddit.html',page=page,posts=posts,type='sub',user={'name':'mozzius'})
 
 @app.route('/r/<subname>/submit')
-def submit(subname):
+@login_required
+def submit(subname,methods=['GET','POST']):
     page = db.getSub(subname)
-    return render_template('roddit.html',page=page,type='submit')
+    if request.method == 'POST':
+        post = request.form
+        fail = db.addPost(post)
+        if fail:
+            return render_template('roddit.html',page=page,type='submit',fail=True)
+    else:
+        return render_template('roddit.html',page=page,type='submit',fail=False)
 
-@app.route('/signin', methods=['GET', 'POST'])
+@login.unauthorized_handler
+def unauthHandler():
+    return redirect(url_for('login'))
+
+@app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         form = request.form
@@ -37,13 +50,32 @@ def login():
             session['email'] = db.user['email']
             return redirect('/')
         else:
-            return render_template('login.html', fail = True)
+            return render_template('roddit.html',type='login',fail=True)
     else:
         if 'loggedin' in session and session['loggedin'] == True:
             return redirect('/')
-        return render_template('login.html', fail=False)
+        else:
+            return render_template('roddit.html',type='login',fail=False)
 
-@app.route('/signout')
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    # doesn't work in the slightest
+    if request.method == 'POST':
+        form = request.form
+        if db.checkLogin(form['email'],form['password']):
+            session['loggedin'] = True
+            session['name'] = db.user['name']
+            session['email'] = db.user['email']
+            return redirect('/')
+        else:
+            return render_template('roddit.html',type='signup',fail=True)
+    else:
+        if 'loggedin' in session and session['loggedin'] == True:
+            return redirect('/')
+        else:
+            return render_template('roddit.html',type='signup',fail=False)
+
+@app.route('/logout')
 def logout():
     session.clear()
     return redirect('/')
@@ -54,7 +86,6 @@ def user(user):
     posts = []
     if user:
         posts = db.getUserPosts(user['_id'])
-        print(posts)
     return render_template('roddit.html',page=user,posts=posts,type='user')
 
 if __name__ == '__main__':
