9ec8fc55d2966a23000504d3f06f5c90f67a9b6c
Mozzius <mozzius.iii@gmail.com>
2018-05-08 09:54:23 +0100

    renamed db

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, redirect, session, request
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user, UserMixin
 import hashlib
-import db
+import database as db
 
 app = Flask(__name__)
 login = LoginManager(app)
